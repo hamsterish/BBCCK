904f09131629f5b01bf19ff463b9f90bf6632b2b
Hamid <hamid.afshar@webfusion.com>
2014-09-18 10:28:02 +0100

    a bit more pod to help the guy who comes along later

--- lib/RomanNumeralGenerator.pm
+++ lib/RomanNumeralGenerator.pm
@@ -1,14 +1,35 @@
 package RomanNumeralGenerator;
 
-=head
-BBC Coding Kata
-Roman Numerals
+
+=head1 NAME
+
+RomanNumeralGenerator - generate Roman numerals from numbers
+
+=head1 DESCRIPTION
+
+A BBC Coding Kata project
+
+=head1 USAGE
+
+use RomanNumeralGenerator;
+
+my $obj = RomanNumeralGenerator->new( number => 10 );
+
+print $obj->generate(); #give you "X"
+
 =cut
 
 use Moose::Util::TypeConstraints;
 use Moose;
 use Modern::Perl;
 use Math::Roman;
+
+=head2 number
+
+Only accept numbers between 1 and 3999. 
+so dont be trying anything out of that range ;)
+
+=cut
 
 has 'number' => (
     is => 'rw',
